0bf52dcf6290ff09c6bf4e7b650a8aa85d7ceba3
markaspot <holger@markaspot.org>
2016-11-11 21:32:41 +0100

    Add a new action side block.

---
+++ src/Plugin/Block/MarkaspotSideBlock.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Drupal\markaspot_blocks\Plugin\Block;
+
+use Drupal\Core\Block\BlockBase;
+/**
+ * Provides a 'Button Action' Block
+ *
+ * @Block(
+ *   id = "markaspot_side_block",
+ *   admin_label = @Translation("Markaspot Side Action block"),
+ * )
+ */
+
+class MarkaspotSideBlock extends BlockBase {
+
+  public function build() {
+    return array(
+      '#type' => 'markup',
+      '#markup' => '
+        <div class="mas-action">
+          <ul class="theme-grey orientation-vertical col-3">
+            <li class="mas-button"><a href="#" role="button" aria-label=""><span class="fa fa-map"></span><span class="">Toggle Map</span></a></li>
+            <li class="mas-button"><a href="/node/add/service_request" data-rel="popup" title="" role="button" aria-label=""><span class="fa fa-plus"></span><span class="add">Add Service request</span></a></li>
+          </ul>
+        </div>
+       '
+    );
+  }
+}
