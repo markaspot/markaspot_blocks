b0ae91516fb852315f38e60dbfaa916eee8d7b79
Holger Kreis <holger@markaspot.org>
2017-03-22 04:38:30 +0100

    Add view button.

--- src/Plugin/Block/MarkaspotSideBlock.php
+++ src/Plugin/Block/MarkaspotSideBlock.php
@@ -24,7 +24,8 @@ class MarkaspotSideBlock extends BlockBase {
         <div class="mas-action">
           <ul class="theme-grey orientation-vertical col-3">
             <li class="mas-button"><a href="#" role="button" aria-label=""><span class="fa fa-map"></span><span class="">Toggle Map</span></a></li>
-            <li class="mas-button"><a href="/report" data-rel="popup" title="" role="button" aria-label=""><span class="fa fa-plus"></span><span class="add">Add Service request</span></a></li>
+            <li class="mas-button"><a href="/report" data-rel="popup" title="" role="button" aria-label=""><span class="fa fa-plus"></span><span class="add">Add service request</span></a></li>
+            <li class="mas-button"><a href="/requests" data-rel="popup" title="" role="button" aria-label=""><span class="fa fa-eye"></span><span class="add">View service requestd</span></a></li>
           </ul>
         </div>
        ',
